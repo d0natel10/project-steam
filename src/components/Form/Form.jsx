46bd3ec438454b7974cfd33041956d9d444e17b3
qeer <lyamtsev03@mail.ru>
2024-06-20 22:49:30 +0300

    as

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,21 +2,25 @@ import React from "react";
 import { useState } from "react";
 import './Form.css';
 
-const Form = () =>{
+const Form = () => {
     const [steamID, setSteamID] = useState('');
-    const [deposit, setDeposit] = useState('50');
+    const [deposit, setDeposit] = useState(50); 
     const [promo, setPromo] = useState('');
 
     const onChangeSteamID = (e) => {
-        setSteamID(e.target.value)
+        setSteamID(e.target.value);
     }
 
     const onChangeDeposit = (e) => {
-        setDeposit(e.target.value)
+        setDeposit(e.target.value);
     }
 
     const onChangePromo = (e) => {
-        setPromo(e.target.value)
+        setPromo(e.target.value);
+    }
+
+    const addToDeposit = (amount) => {
+        setDeposit((prevDeposit) => prevDeposit + amount);
     }
 
     return (
@@ -25,36 +29,38 @@ const Form = () =>{
             <h2>Пополнить Steam</h2>
             <h3>Логин steam</h3>
             <input
-            className={'input'}
-            type="text"
-            placeholder={'Ваш логин steam'}
-            value={steamID}
-            onChange={onChangeSteamID}
+                className={'input'}
+                type="text"
+                placeholder={'Ваш логин steam'}
+                value={steamID}
+                onChange={onChangeSteamID}
             />
             <h3>Сумма пополнения</h3>
             <input
-            className={'input'}
-            type="text"
-            placeholder={'сумма пополнения'}
-            value={deposit}
-            onChange={onChangeDeposit}
+                className={'input'}
+                type="number"
+                placeholder={'сумма пополнения'}
+                value={deposit}
+                onChange={onChangeDeposit}
             />
-            <button className={'button-form'}>
+            <div className={'button-pose'}>
+            <button className={'button-form'} onClick={() => addToDeposit(100)}>
+                +100
+            </button>
+            <button className={'button-form'} onClick={() => addToDeposit(500)}>
                 +500
             </button>
-            <button className={'button-form'}>
+            <button className={'button-form'} onClick={() => addToDeposit(1000)}>
                 +1000
             </button>
-            <button className={'button-form'}>
-                +1500
-            </button>
+            </div>
             <h3>Промокод</h3>
             <input 
-            className={'input'}
-            type="text"
-            placeholder={'Ваш промокод'}
-            value={promo}
-            onChange={onChangePromo}
+                className={'input'}
+                type="text"
+                placeholder={'Ваш промокод'}
+                value={promo}
+                onChange={onChangePromo}
             />
         </div>
     );
